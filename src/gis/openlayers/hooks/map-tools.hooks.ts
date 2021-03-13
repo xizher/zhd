b54c316c3cdd6e4a9d54ad1d1cc9e531cf45413f
xizher <xizher@163.com>
2021-03-13 15:09:18 +0800

    feat: 优化参数配置

--- src/gis/openlayers/hooks/map-tools.hooks.ts
+++ src/gis/openlayers/hooks/map-tools.hooks.ts
@@ -1,7 +1,7 @@
 import { reactive, Ref, ref, watch } from 'vue'
 import { MapTools } from '../plugins/map-tools/map-tools'
 
-function useMapTools (mapTools: MapTools, toolKeys: string[]) : [Ref<string>, string[]] {
+function useMapTools (mapTools: MapTools, toolKeys: string[] = []) : [Ref<string>, string[]] {
   const activedKey = ref(mapTools.activedKey)
   watch(activedKey, k => (k !== mapTools.activedKey) && mapTools.setMapTool(k))
   mapTools.on('change', e => activedKey.value = e.currentKey)
