3ce9488bf98122389a34391dcb9a37879640287f
xizher <xizher@163.com>
2021-03-13 15:09:42 +0800

    feat: vue时间钩子集成

---
+++ src/vue-hooks/time.hooks.ts
@@ -0,0 +1,10 @@
+import { ref, Ref, computed, onUnmounted} from 'vue'
+import { ext } from '../js-exts'
+
+export function useTimeFormat (format = 'yyyy-MM-dd hh:mm:ss') : Ref<string> {
+  const timeStamp = ref(Date.now())
+  const handler = setInterval(() => timeStamp.value = Date.now(), 1000)
+  onUnmounted(() => clearInterval(handler))
+  const timeFormat = computed(() => ext(timeStamp.value).toDateFormat(format))
+  return timeFormat
+}
